f642d47b3e57d21b8d9c080ffdbeaee2bc1a47cd
LiBai666 <overwall2016@gmail.com>
2018-05-19 15:22:36 +0800

    添加退出登录功能

--- Edit/postIssue.js
+++ Edit/postIssue.js
@@ -12,19 +12,13 @@ $("#submit").click(function(){
 
     let title = document.getElementById("title").value
     let body  = document.getElementById("markdownText").value
-    console.log(title)
-
+ 
     let access_token = localStorage._token
     let base_url = "https://api.github.com"
     let posturl = "/repos/gitblog666/program/issues"
-    let a = "?access_token=9a66eff64b3e77c21fddcdc4aedbd7ecf22983d1"
     $.ajax({
         url: base_url+posturl,
         type: 'post',
-        /*header:{
-            Authorization:"token "+access_token,
-            Accept:"application/vnd.github.symmetra-preview+json"
-        },*/
         beforeSend: function(xhr) {
             xhr.setRequestHeader("Authorization","token " + access_token);
         },
